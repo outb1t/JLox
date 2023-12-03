a4be514d8f1cb9d976e529fade0207731a07a6d5
outbid <outbid1990@gmail.com>
2023-12-03 14:14:40 +0200

    if statement implemented

--- src/com/dguru/lox/Interpreter.java
+++ src/com/dguru/lox/Interpreter.java
@@ -24,6 +24,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
+
+    @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
         var value = evaluate(stmt.expression);
         System.out.println(stringify(value));
