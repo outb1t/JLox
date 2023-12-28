9bba92586fcfce812fa1f78ad3edd42a62cdb683
outbid <outbid1990@gmail.com>
2023-12-29 00:14:24 +0200

    closures implemented

--- src/com/dguru/lox/LoxFunction.java
+++ src/com/dguru/lox/LoxFunction.java
@@ -5,9 +5,11 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
 
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
@@ -17,8 +19,7 @@ class LoxFunction implements LoxCallable {
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
-
+        Environment environment = new Environment(closure);
         for(int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
